bd8d5d02efba5b41b69c9b75d0a17e7f815f6e07
Martin Kubík <kubik@outlook.com>
2014-08-16 13:54:43 +0200

    x

--- 01/src/cz/app/SimpleGraph.java
+++ 01/src/cz/app/SimpleGraph.java
@@ -25,7 +25,7 @@ public class SimpleGraph extends Activity {
 
         // init example series data
         GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
-                new GraphViewData(1, 2.0d)
+                new GraphViewData(1, 3.0d)
                 , new GraphViewData(2, 2.0d)
                 , new GraphViewData(2.5, 2.0d) // another frequency
                 , new GraphViewData(3, 2.5d)
